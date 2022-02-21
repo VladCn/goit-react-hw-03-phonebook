7ed516d2cfbe067421000e420ba02ab185846246
VladZ <vladislav.zaytcev84@gmail.com>
2022-02-21 11:54:40 +0200

    start Phonebook save contact in Local Storage

---
+++ src/components/ContactList/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function ContactList({ filter, onFilterContact }) {
+  console.log(filter);
+
+  return (
+    <>
+      <h2>Contacts</h2>
+
+      <label>
+        Find contacts by name
+        <br />
+        <input
+          type="text"
+          name="name"
+          required
+          value={filter}
+          onChange={onFilterContact}
+        />
+      </label>
+    </>
+  );
+}
